700826e896247c13676f2e1985b0ea82fe2dee80
Fabian Tovar <thefabiantovar@gmail.com>
2024-07-24 22:58:30 -0700

    added navigate functions to buttons on main page and courss page. added css to buttons. added css and functionality to list items to add and delete

--- Course_Tracker/src/components/Courses.jsx
+++ Course_Tracker/src/components/Courses.jsx
@@ -1,19 +1,35 @@
 import React, { useState } from "react";
 import styles from "./Courses.module.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Courses() {
   const [inputValue, setInputValue] = useState("");
-  const [courses, setCourses] = useState([]);
+  const [course, setCourses] = useState([]); //array that courses will be added to
 
   function handleChange(e) {
-    setInputValue(e.target.value);
+    setInputValue(e.target.value); //grabs current value in field
   }
 
   function handleSubmit(e) {
-    e.preventDefault();
-    setCourses([...courses, inputValue]);
-    setInputValue("");
+    e.preventDefault(); //stops page reloading
+
+    if (inputValue.trim() === "") return; //prevents empty field
+    setCourses([...course, inputValue]);
+    //^^ updates 'course state', creates new array with all
+    //current 'course' and adds inputValue at end
+    setInputValue(""); //clears input field
   }
+
+  function deleteCourse(courseToDelete) {
+    setCourses(course.filter((course) => course !== courseToDelete));
+  }
+
+  //Navigation of Pages
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
 
   return (
     <>
@@ -25,21 +41,28 @@ export default function Courses() {
             id="userInput"
             placeholder="Enter Course..."
             value={inputValue}
-            onChange={handleChange}
+            onChange={handleChange} //grabs current text in field
           />
-          <button
-            type="submit"
-            className={styles.addBtn}
-            onClick={handleSubmit}
-          >
+          <button type="submit" className={styles.btn} onClick={handleSubmit}>
             Add
           </button>
         </form>
+        <h2>Courses</h2>
         <ul>
-          {courses.map((course) => (
-            <li key={course}>{course}</li>
+          {course.map((course) => (
+            <li key={course} onClick={() => deleteCourse(course)}>
+              {course}
+            </li>
           ))}
         </ul>
+        <div className={styles.navigateBtns}>
+          <button className={styles.btn} id={styles.nvgBtn} onClick={goBack}>
+            Back
+          </button>
+          <button className={styles.btn} id={styles.nvgBtn}>
+            Next
+          </button>
+        </div>
       </div>
     </>
   );
